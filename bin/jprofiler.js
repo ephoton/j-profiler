9efd4c9a75514b281a3c0b4494bb6c92fc5a28bf
ephoton <i@ephoton.cn>
2015-11-23 16:09:21 +0800

    finished function test

---
+++ bin/jprofiler.js
@@ -0,0 +1,57 @@
+;
+(function () {
+    'use strict';
+    var ProfilerStatus;
+    (function (ProfilerStatus) {
+        ProfilerStatus[ProfilerStatus["error"] = 0] = "error";
+        ProfilerStatus[ProfilerStatus["pause"] = 1] = "pause";
+        ProfilerStatus[ProfilerStatus["running"] = 2] = "running";
+        ProfilerStatus[ProfilerStatus["paused"] = 3] = "paused";
+        ProfilerStatus[ProfilerStatus["stopped"] = 4] = "stopped";
+    })(ProfilerStatus || (ProfilerStatus = {}));
+    var root = typeof self == 'object' && self.self === self && self || this;
+    var JProfiler = (function () {
+        function JProfiler() {
+            this._defaultSettings = {};
+        }
+        JProfiler.prototype._run = function (func) {
+            var startTime = root.performance.now();
+            try {
+                func();
+            }
+            catch (error) {
+                console.log(func.name + ': error');
+                return 0;
+            }
+            return root.performance.now() - startTime;
+        };
+        JProfiler.prototype.stop = function () {
+            var a = 1;
+        };
+        JProfiler.prototype.test = function (func, name, repeat) {
+            var repeatTimes = repeat || 1, testStatus = true, sum = 0, report = {
+                name: name || 'anonymous',
+                data: [],
+                avarage: 0,
+                max: 0,
+                min: 0,
+                times: 0
+            };
+            for (var index = 0; index < repeatTimes; index++) {
+                var performance_1 = this._run(func);
+                if (performance_1) {
+                    report.data[index] = performance_1;
+                    sum += performance_1;
+                    report.avarage = sum / (index + 1);
+                    report.max < performance_1 ? report.max = performance_1 : null;
+                    index ? (report.min > performance_1 ? report.min = performance_1 : null) : report.min = performance_1;
+                    report.times++;
+                }
+            }
+            return report;
+        };
+        return JProfiler;
+    })();
+    root.jProfiler = JProfiler;
+})();
+//# sourceMappingURL=jprofiler.js.map
--- lib/jprofiler.ts
+++ lib/jprofiler.ts
@@ -4,6 +4,104 @@
 ;(function() {
 	'use strict';
 	
-	let root = '';
+	interface SettingsInfo {
+		
+	}
 	
-}());
+	// interface OptionsInfo {
+	// 	repeat: number;
+	// }
+	
+	interface ReportInfo {
+		name: string;
+		data: number[];
+		avarage: number;
+		max: number;
+		min: number;
+		times: number;
+	}
+	
+	enum ProfilerStatus {
+		error,
+		pause,
+		running,
+		paused,
+		stopped
+	}
+	
+	let root = typeof self == 'object' && self.self === self && self || this;
+	
+	
+	
+	class JProfiler {
+		
+		
+		
+		private _report: ReportInfo;
+		private	_defaultSettings = {};
+		
+		private _run(func: () => any): number {
+			let startTime = root.performance.now();
+			try {
+				func();
+			}
+			catch (error) {
+				console.log((<any>func).name + ': error');
+				return 0;
+			}
+			
+			return root.performance.now() - startTime;
+		}
+		
+		start: () => {
+			
+		}
+		
+		stop (): any  {
+			var a = 1;
+		}
+
+		test (func: () => any, name?: string, repeat?: number): ReportInfo {
+			let repeatTimes = repeat || 1,
+				testStatus = true,
+				sum = 0,
+				report: ReportInfo = {
+					name: name || 'anonymous',
+					data: [],
+					avarage: 0,
+					max: 0,
+					min: 0,
+					times: 0	
+				};
+			
+			for(let index = 0; index < repeatTimes; index ++) {
+				let performance = this._run(func);
+				if(performance) {
+					report.data[index] = performance;
+					sum += performance;
+					report.avarage = sum / (index + 1);
+					report.max < performance ? report.max = performance : null;
+					index ? (report.min > performance ? report.min = performance : null) : report.min = performance;
+					report.times ++;
+				}
+			}
+			
+			return report;	
+		}
+		
+		compare: () => {
+			
+		}
+				
+		getReport: () => {
+			
+		}
+		
+		
+		
+		
+		
+	}
+	
+	root.jProfiler = JProfiler;
+})();
